5c7a18871316c45ec5ffcb9dfd72950018ff37a6
joseantt <joseant994@gmail.com>
2024-09-24 15:46:08 -0400

    feat: login view and functionality added

--- app/src/main/java/com/example/smalltalk/LoginActivity.java
+++ app/src/main/java/com/example/smalltalk/LoginActivity.java
@@ -1,6 +1,13 @@
 package com.example.smalltalk;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,91 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button mLoginButton;
+    private Button mRegisterButton;
+    private TextInputLayout mEmailEditText;
+    private TextInputLayout mPasswordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mLoginButton = findViewById(R.id.btn_signin);
+        mRegisterButton = findViewById(R.id.btn_register);
+        mEmailEditText = findViewById(R.id.et_email);
+        mPasswordEditText = findViewById(R.id.et_password);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fieldsAreEmpty()){
+                    findViewById(R.id.tv_error).setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                findViewById(R.id.tv_error).setVisibility(View.GONE);
+                login(view);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            changeActivity(MainActivity.class);
+        }
+    }
+
+    public void login(View view) {
+        String email = mEmailEditText.getEditText().getText().toString();
+        String password = mPasswordEditText.getEditText().getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        changeActivity(MainActivity.class);
+                    } else {
+                        showAlert("Error", "Correo o contraseña incorrectos");
+                    }
+                });
+    }
+
+    private <T> void changeActivity(Class<T> activity){
+        Intent intent = new Intent(LoginActivity.this, activity);
+        startActivity(intent);
+        finish();
+    }
+
+    public boolean fieldsAreEmpty() {
+        return mEmailEditText.getEditText().getText().toString().isEmpty()
+                || mPasswordEditText.getEditText().getText().toString().isEmpty();
+    }
+
+    private void showAlert(String title, String message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
+                .show();
     }
 }
