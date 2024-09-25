f81e7710ffdccbaaa63ad916a6e315abd2101505
joseantt <joseant994@gmail.com>
2024-09-24 22:45:40 -0400

    feat: users are able to register

--- app/src/main/java/com/example/smalltalk/LoginActivity.java
+++ app/src/main/java/com/example/smalltalk/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.example.smalltalk;
 
+import static com.example.smalltalk.utils.GeneralUtils.showAlert;
+
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +15,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.example.smalltalk.utils.GeneralUtils;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -36,7 +36,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mLoginButton = findViewById(R.id.btn_signin);
-        mRegisterButton = findViewById(R.id.btn_register);
+        mRegisterButton = findViewById(R.id.register_btn);
         mEmailEditText = findViewById(R.id.et_email);
         mPasswordEditText = findViewById(R.id.et_password);
 
@@ -58,6 +58,13 @@ public class LoginActivity extends AppCompatActivity {
                 login(view);
             }
         });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity(RegisterActivity.class);
+            }
+        });
     }
 
     @Override
@@ -73,13 +80,12 @@ public class LoginActivity extends AppCompatActivity {
         String email = mEmailEditText.getEditText().getText().toString();
         String password = mPasswordEditText.getEditText().getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
+        mAuth.signInWithEmailAndPassword(email.toLowerCase(), password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        FirebaseUser user = mAuth.getCurrentUser();
                         changeActivity(MainActivity.class);
                     } else {
-                        showAlert("Error", "Correo o contraseña incorrectos");
+                        showAlert("Error", "Correo o contraseña incorrectos", this);
                     }
                 });
     }
@@ -91,15 +97,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public boolean fieldsAreEmpty() {
-        return mEmailEditText.getEditText().getText().toString().isEmpty()
-                || mPasswordEditText.getEditText().getText().toString().isEmpty();
+        return mEmailEditText.getEditText().getText().toString().isBlank()
+                || mPasswordEditText.getEditText().getText().toString().isBlank();
     }
 
-    private void showAlert(String title, String message) {
-        new AlertDialog.Builder(this)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
-                .show();
-    }
 }
